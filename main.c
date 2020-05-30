1123eb6f1dccf1867a90c2c2d5292a78b1852cae
luizgonzaga <desconhecido.conhecido@hotmail.com>
2020-05-30 17:01:32 -0300

    0.0.4 - versao arquivo

--- main.c
+++ main.c
@@ -34,29 +34,22 @@ void separaNodes(Linha *no_linha, char linha[100]){
 int main()
 {
 
+    FILE *pont_arq;
     ArvBin* raiz = cria_ArvBin();
     Linha no_linha;
+    char linha[100];
 
-    //fgets(linha,10,stdin);
-    char linha1[] = "A1,B1,B2";
-    char linha2[] = "B1,C1,X";
-    char linha3[] = "B2,X,S1";
-    char linha4[] = "C1,D1,D2";
+    pont_arq = fopen("dados.txt", "r");
 
-    separaNodes(&no_linha, linha1);
-    insere_ArvBin(raiz, no_linha);
-
-    separaNodes(&no_linha, linha2);
-    insere_ArvBin(raiz, no_linha);
-
-    separaNodes(&no_linha, linha3);
-    insere_ArvBin(raiz, no_linha);
-
-    separaNodes(&no_linha, linha4);
-    insere_ArvBin(raiz, no_linha);
+    while(fgets(linha, 100, pont_arq) != NULL){
+        printf("%s\n",linha);
+        separaNodes(&no_linha, linha);
+        insere_ArvBin(raiz, no_linha);
+    }
 
     visitaNodes(raiz);
 
+    fclose(pont_arq);
     system("Pause");
 
     return 0;
