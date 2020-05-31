be3b9929354d357a12a330cd83b0411ff59891e6
luizgonzaga <desconhecido.conhecido@hotmail.com>
2020-05-31 16:21:50 -0300

    0.0.6 - versao arquivo final

--- ArvoreBinaria.c
+++ ArvoreBinaria.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "ArvoreBinaria.h" //inclui os Protótipos
 
+int TNv_esq=0, TNv_dir=0;
+
 struct NO{
     char info[100];
     struct NO *esq;
@@ -73,7 +75,9 @@ int insere_ArvBin(ArvBin* raiz, Linha no_linha){
         ArvBin* sub_dir = cria_ArvBin();
 
         // se nao encontra valor entra em recursao
-        if (strcmp(no_linha.raiz, atual->info)){
+        //printf("no_linha.raiz: %s(%d), atual->info: %s(%d)\n",no_linha.raiz, strlen(no_linha.raiz), atual->info, strlen(atual->info));
+        int nao_achou = strcmp(no_linha.raiz, atual->info);
+        if (nao_achou){
             *sub_esq = atual->esq;
             *sub_dir = atual->dir;
             insere_ArvBin(sub_esq, no_linha);
@@ -101,21 +105,28 @@ int insere_ArvBin(ArvBin* raiz, Linha no_linha){
 }
 
 
-void visitaNodes(ArvBin* raiz){
+void visitaNodes(ArvBin* raiz, int *pTNv, int *pTSd, int *pTNd){
     struct NO* atual = *raiz;
     ArvBin* sub_esq = cria_ArvBin();
     ArvBin* sub_dir = cria_ArvBin();
-    char node[50];
-    char esqDir[50];
-    int i=0,qtd_nodes=0;
+    char node[50], esqDir[50];
+    int i=0, qtd_nodes=0;
 
     // avalia a raiz
     if (*raiz==NULL){
+        if (TNv_esq > *pTNv)
+            *pTNv = TNv_esq;
+        else if (TNv_dir > *pTNv)
+            *pTNv = TNv_dir;
+
+        TNv_esq=0;
+        TNv_dir=0;
+
         return;
     }
 
+    *pTNd = *pTNd + 1;
     strcpy(node, atual->info);
-
     // avalia sub-arvore esquerda
     if (atual->esq!=NULL){
         esqDir[i]='E';
@@ -124,7 +135,6 @@ void visitaNodes(ArvBin* raiz){
         qtd_nodes++;
         *sub_esq = atual->esq;
     }
-
     // avalia sub-arvore direita
     if (atual->dir!=NULL){
         esqDir[i]='D';
@@ -133,13 +143,25 @@ void visitaNodes(ArvBin* raiz){
         qtd_nodes++;
         *sub_dir = atual->dir;
     }
-    if (qtd_nodes==0)
+    // se nao possui nodes entao eh um no folha
+    if (qtd_nodes==0){
         esqDir[i]='F';
+    }
 
-    // remove caracter invalido
-    int n = strlen(node);
-    node[n-1]='\0';
+
+    // numero de nos saida("S")
+    if (node[0]=='S')
+        *pTSd = *pTSd + 1;
+
     printf("%s %d %s\n", node, qtd_nodes, esqDir);
-    visitaNodes(sub_esq);
-    visitaNodes(sub_dir);
+    TNv_esq = TNv_esq + 1;
+    visitaNodes(sub_esq, pTNv, pTSd, pTNd);
+    TNv_dir = TNv_dir + 1;
+    visitaNodes(sub_dir, pTNv, pTSd, pTNd);
+
+    // obtem a direcao mais profunda e soma raiz
+
+
+
+
 }
--- main.c
+++ main.c
@@ -3,23 +3,23 @@
 #include "ArvoreBinaria.h"
 
 
-void separaNodes(Linha *no_linha, char linha[100]){
+void separaNodes(Linha *no_linha, char linha[1024]){
     int n = strlen(linha);
     int j=0, pos_linha=1;
     for (int i=0; i<n; i++){
         if (linha[i]!=',' && pos_linha==1){
             no_linha->raiz[j] = linha[i];
-            no_linha->raiz[j+1] = "\0";
+            no_linha->raiz[j+1] = '\0';
             j++;
         }
         else if (linha[i]!=',' && pos_linha==2){
             no_linha->esq[j] = linha[i];
-            no_linha->esq[j+1] = "\0";
+            no_linha->esq[j+1] = '\0';
             j++;
         }
         else if (linha[i]!=',' && pos_linha==3){
             no_linha->dir[j] = linha[i];
-            no_linha->dir[j+1] = "\0";
+            no_linha->dir[j+1] = '\0';
             j++;
         }
         else {
@@ -38,7 +38,7 @@ int main(){
     Linha no_linha;
     char linha[1024];
 
-    pont_arq = fopen("dados.txt", "r");
+    pont_arq = fopen("dados1.txt", "r");
 
     while(fgets(linha, sizeof(linha), pont_arq) != NULL){
         /* Remove a nova linha (\n), caso ela tenha sido lida pelo fgets */
@@ -50,7 +50,11 @@ int main(){
         insere_ArvBin(raiz, no_linha);
     }
 
-    visitaNodes(raiz);
+    int TNv=0, TSd=0, TNd=0;
+    visitaNodes(raiz, &TNv, &TSd, &TNd);
+    printf("TNv %d\n",TNv);
+    printf("TSd %d\n",TSd);
+    printf("TNd %d\n",TNd);
 
     fclose(pont_arq);
     system("Pause");
